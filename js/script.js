850fc48a9f73dab59a6423b420dc9abb0e437fd8
Astrates <st.kostow@gmail.com>
2022-06-11 11:51:05 +0300

    update

--- js/script.js
+++ js/script.js
@@ -11,44 +11,75 @@ function clearLast(){
    
    ////////////////// Change value on the Display //////////////////
 
-                   ////////// Symbol restrictions //////////
    function display(value) {
         let store = document.getElementById("result").value += value;
+        onlyOneDot(store);
+        SymbolRestrict(store);
+        if (store.length > 1){
+        CheckExtraSymbols(store);
+        }
+       }
+
+     ////////////////// Checks symbol in the [0] //////////////////  
+
+    function SymbolRestrict(store){
         let compare = store[0];
-        let repeat = store[1];
         switch(compare){
             case "+":
             case "/":
             case "*":
             document.getElementById("result").value = "";
             break;
-            case "-": 
-                switch(repeat){
-                    case "+":
-                    case "/":
-                    case "*":
-                    case "-":
-                        clearLast();
-                        break; 
                 }
        }
-                    ////////// No more than one dot(.) /////////
-       let last = value[value.length-1];
-           switch(last){
-            case ".":
-                   document.getElementById("dot").style = "display:none;"
-                   break;
+       
+     ////////////////// Checks for repeating symbols and /0 //////////////////  
+
+    function CheckExtraSymbols(store){
+        let check1 = store[store.length-2]
+        let check2 = store[store.length-1]
+        switch(check1){
             case "+":
             case "-":
             case "/":
+                if (check2 == 0){
+                    alert("You can't divide by 0 young one.");
+                    clearLast();
+                }
             case "*":
+            case ".":
+                switch(check2){
+                    case "+":
+                    case "-":
+                    case "/":
+                    case "*":
+                        clearLast();
+                        break; 
+                }
+            break;
+        }
+    }
+     
+      ////////////////// Controls the dots //////////////////  
+
+    function onlyOneDot(store){
+        let last = store[store.length-1];
+         switch(last){
+         case ".":
+                document.getElementById("dot").style = "display:none;"
+                break;
+         case "+":
+          case "-":
+         case "/":
+          case "*":
+            if(store[store.length-2] != ".")
                     document.getElementById("dot").style = "display:visible;"
-                    break;
-
-           }
-       }
-
+                   break;
+          }
+     }
+       
    //////////////////// Calculate the value //////////////////////
+
    function operate() {
     let p = document.getElementById("result").value;
     let q = eval(p);
